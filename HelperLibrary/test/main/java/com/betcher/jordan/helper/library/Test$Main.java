9fcdb582e4f801e486f021f8d3b9cf36fa62b0f5
Plum <pplluumm@users.noreply.github.com>
2021-09-16 07:47:10 -0700

    Setup test folder

---
+++ HelperLibrary/test/main/java/com/betcher/jordan/helper/library/Test$Main.java
@@ -0,0 +1,14 @@
+package main.java.com.betcher.jordan.helper.library;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Test$Main
+{
+	@Test
+	public void runs()
+	{
+	
+	}
+}
