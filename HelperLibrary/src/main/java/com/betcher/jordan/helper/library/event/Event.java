228143312825782b90c6256732bb277598d6eff9
Plum <pplluumm@users.noreply.github.com>
2021-09-17 13:55:05 -0700

    t Event callAdd_listener_call

--- HelperLibrary/src/main/java/com/betcher/jordan/helper/library/event/Event.java
+++ HelperLibrary/src/main/java/com/betcher/jordan/helper/library/event/Event.java
@@ -1,6 +1,23 @@
 package main.java.com.betcher.jordan.helper.library.event;
 
+import java.util.List;
+
 public class Event
 {
-
+	
+	
+	public void add(Listener listener)
+	{
+		listener.call();
+	}
+	
+	public void remove(Listener listener)
+	{
+	
+	}
+	
+	public void call()
+	{
+	
+	}
 }
---
+++ HelperLibrary/test/main/java/com/betcher/jordan/helper/library/event/Test$Event.java
@@ -0,0 +1,21 @@
+package main.java.com.betcher.jordan.helper.library.event;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class Test$Event
+{
+	@Test
+	public void callAdd_listener_call()
+	{
+		Listener listener = mock(Listener.class);
+		
+		Event event = new Event();
+		event.add(listener);
+		event.call();
+		
+		verify(listener, times(1)).call();
+	}
+}
