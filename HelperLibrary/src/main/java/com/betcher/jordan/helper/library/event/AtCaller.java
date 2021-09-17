c57f10c7b5ce3326a48664afa87cbdc858340f4d
Plum <pplluumm@users.noreply.github.com>
2021-09-17 12:05:11 -0700

    c init AtCaller

---
+++ HelperLibrary/src/main/java/com/betcher/jordan/helper/library/event/AtCaller.java
@@ -0,0 +1,29 @@
+package main.java.com.betcher.jordan.helper.library.event;
+
+public class AtCaller
+{
+	public AtCaller(OnCaller<Void> onCaller, AtToOn atToOn)
+	{
+	
+	}
+	
+	public void add(At at)
+	{
+	
+	}
+	
+	public void add(At at, int numberOfCalls)
+	{
+	
+	}
+	
+	public void call()
+	{
+	
+	}
+	
+	public void remove(At at)
+	{
+	
+	}
+}
---
+++ HelperLibrary/test/main/java/com/betcher/jordan/helper/library/event/Test$AtColler.java
@@ -0,0 +1,10 @@
+package main.java.com.betcher.jordan.helper.library.event;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Test$AtColler
+{
+	
+}
